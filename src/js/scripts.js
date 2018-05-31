64aaa2283137d2129ad48feaa7ae1cca3e23d3ff
Harry Cresswell <studio@harrycresswell.com>
2018-05-31 13:23:27 +0200

    fixed algolia search

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,60 @@ const me = whoAmI()
 
 console.log("Hello world! I'm " + me)
 console.log("Find me in src/js/scripts.js")
+
+// Set Algolia options
+const options = {
+  appId: 'B0AX7GJQ2W',
+  apiKey: '1ba66e5f7ccc7b6be26026ea9c8c6f35',
+  indexName: 'hugoalgolia',
+  hitsPerPage: 10,
+  routing: true
+}
+
+// Parse options to instantsearch
+const search = instantsearch(options);
+
+// initialize SearchBox
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: 'Search for post',
+    reset: false,
+    cssClasses: {
+     root: 'Search-box__container',
+     input: 'Search-box__input',
+    }
+  })
+);
+
+// create variable for custom hit template
+var hitTemplate =
+  '<a href="{{ relpermalink }}" class="List__item">' +
+        '<div class="List-title">{{{_highlightResult.title.value}}}</div>' +
+  '</a>' + '<div class="List-summary">{{{summary}}}</div>';
+
+// initialize hits widget
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    cssClasses: {
+      root:'Search-hits',
+      empty:'Search-hits--empty'
+    },
+    templates:{
+      // call custom hit template
+      item: hitTemplate,
+      empty: 'Didn’t find any results for the search <em>\"{{query}}\"</em>'
+    }
+  })
+);
+
+// initialize RefinementList
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#refinement-list',
+    attributeName: 'categories'
+  })
+);
+
+search.start();
